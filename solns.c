ba03e733bab07861c221a5fe40b3482e551b9b13
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2021-06-13 12:27:34 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -14,7 +14,7 @@ int max(int a[], int n)
 int min(int a[], int n)
 {
    int min =a[0];
-   for(i=1;i<n;i++)
+   for(int i=1; i<n; i++)
    {
       if(a[i]<min)
          min=a[i];
@@ -25,7 +25,7 @@ int min(int a[], int n)
 float average(int a[], int n)
 {
    float sum =0;
-   for(i=0; i<n; i++)
+   for(int i=0; i<n; i++)
    {
       sum += a[i];
    }
@@ -39,12 +39,12 @@ int factors(int a[], int n)
 int mode(int a[], int n)
 {
    int c[10]={0,0,0,0,0,0,0,0,0,0};
-   for(i=0; i<n; i++)
+   for(int i=0; i<n; i++)
    {
       c[a[i]]++;
    }
-   max = c[0];
-   for(j=1; j<n; j++)
+   int max = c[0];
+   for(int j=1; j<n; j++)
    {
      if(c[j] > max)
         max = c[j];
