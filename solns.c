d7296c3da92ae52d136f442ee39ba599b74a71ab
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2021-06-13 17:28:04 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -39,17 +39,28 @@ int factors(int n, int a[])
 
 int mode(int a[], int n)
 {
-   int c[10];
-   int i,j,k;
-   for(i=0; i<n; i++)
+   
+   int i,j,k,max;
+   max =a[0];
+   for(i=1; i<n; i++)
+   {
+      if(a[i] > max)
+         max = a[i];
+   }
+   int counts[max+1];
+   for(i=0;i<=max;i++)
+   {
+      counts[i] = 0;
+   }
+   for(i=0;i<n;i++)
    {
       k = a[i];
-      (c[k])++;
+      counts[k]++;
    }
-   int max = c[0];
+   max = counts[0];
    for(j=1; j<n; j++)
    {
-     if(c[j] > max)
+     if(counts[j] > max)
         max = c[j];
    }
    return j;
